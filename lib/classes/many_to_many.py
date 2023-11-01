75bee7856a8674cad86971a22a5fd335cbdfef40
stephenlambertfs <stephenolambert@gmail.com>
2023-11-01 11:24:25 -0600

    finished solution

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,104 @@
 class Game:
+    all = []
     def __init__(self, title):
         self.title = title
+        Game.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str) and not hasattr(self, "title"):
+            self._title = title
+
 
     def results(self):
-        pass
+        return [result for result in Result.all if result.game is self]
 
     def players(self):
-        pass
+        return list({result.player for result in self.results()})
 
     def average_score(self, player):
-        pass
+        return sum([result.score for result in self.results()]) / len(self.results())
 
-class Player:
+class Player(Game):
+    all = []
     def __init__(self, username):
         self.username = username
+        Player.all.append(self)
+
+
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and 2 <= len(username) <= 16:
+            self._username = username
+
 
     def results(self):
-        pass
+        return [result for result in Result.all if result.player is self]
+    
 
     def games_played(self):
-        pass
+        return list({result.game for result in self.results()})
 
     def played_game(self, game):
-        pass
+        return game in self.games_played()
 
     def num_times_played(self, game):
-        pass
+        played = [result.game for result in self.results()]
+        return played.count(game)
+    
+    @classmethod
+    def highest_scored(cls, game):
+        players = [player for player in cls.all if player.played_game(game)]
+
+        if not players:
+            return None
+        
+        high_score = max(players, key = lambda player: player.average_score(game))
+
+        return high_score
 
 class Result:
+    all = []
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
+        Result.all.append(self)
+
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def score(self, score):
+        if isinstance(score, int) and not hasattr(self, "score") and 1 <= score <= 5000:
+            self._score = score
+    
+    @property
+    def player(self):
+        return self._player
+
+    @player.setter
+    def player(self, player):
+        if isinstance(player, Player):
+            self._player = player
+    @property
+    def game(self):
+        return self._game
+
+    @game.setter
+    def game(self, game):
+        if isinstance(game, Game):
+            self._game = game
+
+
+    
+
--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,15 @@ from classes.many_to_many import Result
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+    player_1 = Player("Alice")
+    player_2 = Player("Bob")
+
+    game = Game("Chess")
+    result_1 = Result(player_1, game, 1000)
+    result_2 = Result(player_2, game, 500)
+
+    highest_scored_player = Player.highest_scored(game)
+
+    print(highest_scored_player)
+
     ipdb.set_trace()
